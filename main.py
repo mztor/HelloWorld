e5654227f58c5fa9e378005341c1fe483e438dd4
mztor <stor@macarthur.nsw.edu.au>
2024-03-20 12:37:40 +1100

    Keep printing based on random number of "o"

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import random
 print("Hello World!")
 #adjust the program to print Hello World with
 #a random amount of "o"'s in Hello
 
+ans = input("Print again? ")
+
+while ans != "":
+    oNum = random.randint(1,25)
+    print(f"Hell{oNum * 'o'} World!")
+    ans = input("Print again? ")
